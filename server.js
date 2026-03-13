03a6fd06371b2326b94bbc846cfd6acc5a20aa13
panneela <anumalapanneela@gmail.com>
2026-03-13 20:34:09 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express=require("express");
+
+const mongoose=require("mongoose");
+
+const bodyParser=require("body-parser");
+
+const studentRoutes=require("./routes/studentRoutes");
+
+const facultyRoutes=require("./routes/facultyRoutes");
+
+const app=express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/6csn1")
+
+.then(()=>console.log("MongoDB Connected"))
+
+.catch(err=>console.log(err));
+
+app.use("/api",studentRoutes);
+
+app.use("/api",facultyRoutes);
+
+app.listen(3000,()=>{
+
+    console.log("Server running on port 3000");
+
+});
